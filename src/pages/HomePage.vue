4b30d6438827bf70150a487802f2a5b1c4d136e4
Andrew Allison <andrewallison5211@gmail.com>
2023-09-11 21:27:38 -0600

    starting

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="background-img">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-6">
+          <div class="ms-5 mt-5">
+            <h1 class="fs-5 text-white">Hi, I'm Andrew</h1>
+            <h1 class="fs-3">A full-stack <h2 class="">Software Developer</h2></h1>
+          </div>
+        </div>
+
+        <div class="col-6"></div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.background-img{
+  background-image: url(https://images.hdqwalls.com/wallpapers/minimalistic-shapes-dark-neon-4k-54.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
+</style>
