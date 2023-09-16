6bb851d43d145ff6f5d55d7e66d4ee6b78d8b73b
Andrew Allison <andrewallison5211@gmail.com>
2023-09-15 23:48:05 -0600

    tower added

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -20,11 +20,20 @@
             </div>
 
             <div class="card ms-2" style="width: 22rem;">
-              <img src='../assets/img/Screenshot (1).png' class="card-img-top" alt="Keepr">
+              <img src='../assets/img/Screenshot (2).png' class="card-img-top" alt="Devs and Dragons">
               <div class="card-body text-center">
                 <h5 class="card-title">Devs and Dragons</h5>
                 <p class="card-text">Devs and Dragons is a D&D Campaign management application that allows players to easily track and coordinate the status of their game. It was built in a team of five devolpers over the course of two weeks using a VueJs front end and NodeJs back end. This application also integrates the D&D api in the form of a search funtion present in each campaign.</p>
                 <a class="text-center" href="https://github.com/AndrewAllison2/devs_and_dragons">View Github Repo</a>
+              </div>
+            </div>
+
+            <div class="card ms-2" style="width: 22rem;">
+              <img src='https://images.unsplash.com/photo-1612358405970-e1aeba9d76c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&h=500&q=80' class="card-img-top" alt="Tower">
+              <div class="card-body text-center">
+                <h5 class="card-title">Tower</h5>
+                <p class="card-text">Tower is a web application built with a VueJs front end and a NodeJs back end. It is centered around events called Towers. Users can get tickets to events they want to attend and leave comments on towers they are attending or are interested in.</p>
+                <a class="text-center" href="https://github.com/AndrewAllison2/Tower">View Github Repo</a>
               </div>
             </div>
 
@@ -46,5 +55,6 @@ export default {
 img {
   height: 10em;
   object-fit: cover;
+  padding: 2px;
 }
 </style>
