80c9592e5504c88a953ed079844ff21cb35a8d21
Andrew Allison <andrewallison5211@gmail.com>
2023-09-27 16:53:08 -0600

    apps

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -24,27 +24,76 @@
 
       <div class="row">
         <h2 class="fs-1 text-white text-center mb-5">APPLICATIONS</h2>
-        <!-- <AppComponent /> -->
-        <div class="col-6 d-flex">
-          <div class="pb-5">
-            <img class="img-fluid dd-img ms-4" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
-          </div>
-        </div>
-        
-        <div class="col-6">
-          <div class="text-content-1 text-white ms-5 card elevation-4">
+        <!-- <div class="col-6">
+          <div class="text-content-1 text-white ms-3 card elevation-4">
             <h1 class="text-center p-1">Devs And Dragons</h1>
             <div class="p-2">
-              <p class="text-center">Devs and Dragons is a D&D campaign management application that helps players seamlessly manage and track their adventures in their game. Built by a team of five developers over the course of two weeks, this application was built with a VueJs front end and a NodeJS/Express back end. It integrates our own API as well as the official D&D API, which were both tested with Postman. It includes a wide array of technologies and features such as child routing to navigate and render important data while keeping focus on the game, intuitive and flexible UI based on a users status within their campaign, and account information tracking content you have created across all your adventures.
+              <p class="text-center">Devs and Dragons is a D&D campaign management application that helps players seamlessly manage and track their adventures in their game. Built by a team of five developers over the course of two weeks, this application was built with a VueJs front end and a NodeJS/Express back end. It integrates our own API as well as the official D&D API, which were both tested with Postman. It includes a wide array of technologies and features such as child routing to navigate and render important data while keeping focus on the game, intuitive and flexible UI based on a users status within their campaign, and authentication to track information and content you have created across all your adventures.
               </p>
               <p>
-                Our team designed, created, and utilized our own UML, Figma mocks, and a Trello board throughout the build process. We also observed Agile and Scrum practices. We were faced with challeneges throughout the process, but strict project management, communication, and coordination allowed us to minimize merge conflicts and maximize our efficiency, which was a key factor in finishing the application within the time frame. 
+                Our team designed, created, and utilized our own UML, Figma mocks, and a Trello board throughout the build process. We also observed Agile and Scrum practices. We were faced with challeneges during the process, but strict project management, communication, and coordination allowed us to minimize merge conflicts and maximize our efficiency, which was a key factor in finishing the application within the time frame. 
               </p>
             </div>
           </div>
         </div>
-        
+
+        <div class="col-6 d-flex">
+          <div class="pb-5">
+            <img class="img-fluid dd-img" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
+          </div>
+        </div> -->
       </div>
+
+      <!-- SECTION DnD App -->
+      <div class="row">
+        <h1 class="text-center mb-3 text-white">Devs And Dragons</h1>
+        <div class="col-4 dd-home-container">
+          <div class="pb-5">
+            <img class="img-fluid dd-home-img" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
+            <div class="overlay-dd-home">
+              <p class=" home-text p-3">Devs and Dragons is a D&D campaign management application that helps players seamlessly manage and track their adventures in their game. Built by a team of five developers over the course of two weeks, this application was built with a VueJs front end and a NodeJS/Express back end. It integrates our own API as well as the official D&D API, which were both tested with Postman. This application utilizes a variey of technologies, an intutive and flexible UI, and was built with the potential for scaling in mind.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 dd-home-container">
+          <div class="pb-5">
+            <img class="img-fluid dd-home-img" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
+            <div class="overlay-dd-home">
+              <p class=" home-text p-3">Devs and Dragons is a D&D campaign management application that helps players seamlessly manage and track their adventures in their game. Built by a team of five developers over the course of two weeks, this application was built with a VueJs front end and a NodeJS/Express back end. It integrates our own API as well as the official D&D API, which were both tested with Postman. This application utilizes a variey of technologies, an intutive and flexible UI, and was built with the potential for scaling in mind.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 dd-home-container">
+          <div class="pb-5">
+            <img class="img-fluid dd-home-img" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
+            <div class="overlay-dd-home">
+              <p class=" home-text p-3">Devs and Dragons is a D&D campaign management application that helps players seamlessly manage and track their adventures in their game. Built by a team of five developers over the course of two weeks, this application was built with a VueJs front end and a NodeJS/Express back end. It integrates our own API as well as the official D&D API, which were both tested with Postman. This application utilizes a variey of technologies, an intutive and flexible UI, and was built with the potential for scaling in mind.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <a class="btn btn-primary dnd-btn" href="https://github.com/AndrewAllison2/devs_and_dragons" title="To GitHub/Devs&Dragons" role="button">View Code</a>
+      </div>
+
+      <!-- SECTION Keepr -->
+      <div class="row">
+        <h1 class="text-center mb-3 text-white">Keepr</h1>
+        <div class="col-5 m-auto keepr-container">
+          <div>
+            <img class="img-fluid keepr-home" src="https://figmage.com/images/KndD8m4gDG7hjt1xgq5lo.jpg" alt="Keepr Picture">
+          </div>
+        </div>
+        <div class="col-5"></div>
+      </div>
+
+      <!-- SECTION AllSpice -->
+      <div class="row">
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -69,23 +118,55 @@ export default {
   background-attachment: fixed;
 }
 
-.avatar {
-  object-fit: cover;
-  
+.dnd-btn {
+  width: 9em;
+  position: relative;
+  left: 43em;
+  top: -1em;
 }
 
-.dd-img {
-  // width: 33em;
-  object-fit: cover;
-  border-radius: 2px;
-  // position: relative;
-  // right: -4em;
+.dd-home-container {
+  position: relative;
 }
 
-.text-content-1 {
-  background-color: rgba(63, 63, 63, 0.418);
-  
-  
+.dd-home-img {
+  object-fit: cover;
+  border-radius: 5px;
+}
+
+.overlay-dd-home {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 10px;
+  right: 0;
+  height: 85%;
+  width: 95.7%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #272727;
+  border-radius: 5px;
+}
+
+.dd-home-container:hover .overlay-dd-home {
+  opacity: 1;
+}
+
+.home-text {
   color: white;
+  position: absolute;
 }
+
+.keepr-container{
+  position: relative;
+}
+
+.keepr-home {
+  object-fit: cover;
+  border-radius: 5px;
+}
+
+
+
+
 </style>
