910b2d275454e03657961c412ae413b5ada21bf7
Andrew Allison <andrewallison5211@gmail.com>
2023-09-27 16:06:46 -0600

    landing page changes

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -12,7 +12,7 @@
 
         <div class="col-6">
           <div>
-            <img src="" alt="Andrew Allison">
+            <!-- <img class="avatar" src="../assets/img/Professional Photo.jpg" alt="Andrew Allison"> -->
           </div>
         </div>
 
@@ -25,10 +25,22 @@
       <div class="row">
         <h2 class="fs-1 text-white text-center mb-5">APPLICATIONS</h2>
         <!-- <AppComponent /> -->
-        <div class="col-12">
-
+        <div class="col-6 d-flex">
           <div class="pb-5">
-            <img class="img-fluid dd-img" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
+            <img class="img-fluid dd-img ms-4" src='../assets/img/Screenshot (2).png' alt="Devs and Dragons">
+          </div>
+        </div>
+        
+        <div class="col-6">
+          <div class="text-content-1 text-white ms-5 card elevation-4">
+            <h1 class="text-center p-1">Devs And Dragons</h1>
+            <div class="p-2">
+              <p class="text-center">Devs and Dragons is a D&D campaign management application that helps players seamlessly manage and track their adventures in their game. Built by a team of five developers over the course of two weeks, this application was built with a VueJs front end and a NodeJS/Express back end. It integrates our own API as well as the official D&D API, which were both tested with Postman. It includes a wide array of technologies and features such as child routing to navigate and render important data while keeping focus on the game, intuitive and flexible UI based on a users status within their campaign, and account information tracking content you have created across all your adventures.
+              </p>
+              <p>
+                Our team designed, created, and utilized our own UML, Figma mocks, and a Trello board throughout the build process. We also observed Agile and Scrum practices. We were faced with challeneges throughout the process, but strict project management, communication, and coordination allowed us to minimize merge conflicts and maximize our efficiency, which was a key factor in finishing the application within the time frame. 
+              </p>
+            </div>
           </div>
         </div>
         
@@ -38,13 +50,13 @@
 </template>
 
 <script>
-import AppComponent from "../components/AppComponent.vue";
+
 
 export default {
     setup() {
         return {};
     },
-    components: { AppComponent }
+
 }
 </script>
 
@@ -57,10 +69,23 @@ export default {
   background-attachment: fixed;
 }
 
-.dd-img {
-  width: 33em;
+.avatar {
   object-fit: cover;
-  position: relative;
-  right: -4em;
+  
+}
+
+.dd-img {
+  // width: 33em;
+  object-fit: cover;
+  border-radius: 2px;
+  // position: relative;
+  // right: -4em;
+}
+
+.text-content-1 {
+  background-color: rgba(63, 63, 63, 0.418);
+  
+  
+  color: white;
 }
 </style>
