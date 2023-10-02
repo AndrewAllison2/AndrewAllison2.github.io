859f1195b0831c5564b25214276582e4fcad0127
Andrew Allison <andrewallison5211@gmail.com>
2023-10-02 12:29:36 -0600

    new page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-
+  <div class="container-fluid bg-black">
+    <div class="row mb-5">
+      <div class="col-10 m-auto mt-5 me-5">
+        <div class="mt-5 ms-5 text-white">
+          <h1 class="fs-5">Hi, I'm Andrew</h1>
+          <h1 class="fs-5">A full stack <span class="fs-1 text-warning">Software Developer</span></h1>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +24,9 @@ export default {
 
 <style scoped lang="scss">
 
+.container {
 
+}
 
 
 
