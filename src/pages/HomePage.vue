ef28232d97f7bee26b08ff6b622c37b802c870f7
Andrew Allison <andrewallison5211@gmail.com>
2023-10-24 12:52:16 -0600

    big refactor

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,153 +9,189 @@
       </div>
   
       <div class="row mb-5">
-        <div class="col-8 m-auto mt-5 me-5">
+        <div class="col-6 m-auto mt-5 me-5">
           <div class="mt-5 ms-5 text-black typewriter">
-            <h1 class="fs-5">Hi, I'm Andrew</h1>
-              <h1 class="fs-5">A full stack <span class="fs-1 text-info">Software Developer</span></h1>
+            <h1 class="fs-5 mt-5 text-shadow">Hi, I'm Andrew</h1>
+              <h1 class="fs-5 text-shadow">A full stack <span class="fs-1 text-info">Software Developer</span></h1>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-10 m-auto my-5">
         <div class="h-line"></div>
       </div>
-    </div>
+    </div> -->
 
-    <div class="row">
-      <div class="col-8 m-auto my-5">
-        <h2 class="fs-1 text-white text-center">Applications</h2>
+    <div class="background-sec">
+      <div class="row">
+      <div class="col-5 text-center my-5">
+        <img class="img-fluid avatar" src="../assets/img/Professional Photo.jpg" alt="Andrew Allison">
+      </div>
 
-        <div class="d-flex justify-content-between text-warning mt-5">
-          <h3 class="selectable" title="Devs & Dragons">Devs & Dragons</h3>
-          <h3 class="selectable" title="Keepr">Keepr</h3>
-          <h3 class="selectable" title="AllSpice">AllSpice</h3>
+      <div class="col-5 my-5">
+        <div class="text-white">
+          <h2 class="mb-4">About Me</h2>
+          <h5 class="mt-3 mb-3 font-italic">Hi, I'm Andrew!</h5>
+          <p>I'm a passionate creator with a knack for design and problem solving. I enjoy designing and building intuitve UI's, as well as dealing with complex logic and data management. I thouroghly enjoy being challenged and working through problems to find creative solutions.</p>
+          
+          <p>Some of my hobbies include writing music, traveling to new places, and spending time outdoors with my wife and our golden retriever. I love learning new things and expanding my knoweldge in fields that I find interesting.</p>
         </div>
       </div>
     </div>
 
-<!-- NOTE have it cycle through multiple imgs of apps here would be cool -->
-    <div class="row mt-3" id="devs&dragons">
-      <div class="col-6">
-        <div class="ms-4 mb-5">
-          <img class="img-fluid" src="../assets/img/Screenshot (2).png" alt="Devs & Dragons">
-        </div>
-      </div>
-
-      <div class="col-6">
-        <div class="me-4 mb-3 text-center text-white">
-          <h3>Devs & Dragons</h3>
-          <p>Devs & Dragons is a DnD campaign management application designed to create seamless coordination between players. This application was built over the course of two weeks by a team of five developers. Our build process included the design and utilization of our own Figma mocks, data UML, and Trello board, and heavy coordination and collaboration across the back and front end.  </p>
-
-          <p>We observed Agile and Scrum practices throughout the project. Our coordination allowed us to minimize errors and conflicts, and maximize our efficiency, allowing us to complete this large and complex application within the time frame.</p>
-        </div>
-
-        <div class="sm-h-line"></div>
-
-        <div class="d-flex justify-content-between align-items-center mx-2 mt-4">
-          <img class="img-fluid html-icon" src="https://cdn3d.iconscout.com/3d/free/thumb/free-html-5728485-4781249.png" alt="HTML" title="HTML5">
-          <img class="img fluid css-icon" src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" title="CSS3">
-          <img class="img fluid js-icon" src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript" title="JavaScript">
-          <img class="img fluid vue-icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/367_Vuejs_logo-512.png" alt="VueJs" title="VueJs">
-          <img class="img fluid node-icon" src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" alt="NodeJs" title="NodeJS/Express">
-        </div>
-      </div>
     </div>
 
-        <div class="row">
-          <div class="col-12 m-auto my-5">
-            <div class="full-h-line"></div>
+    <div class="background-third">
+      <div class="row">
+        <div class="col-8 m-auto my-5">
+          <h2 class="fs-1 text-white text-center">Applications</h2>
+  
+          <div class="d-flex justify-content-between text-warning mt-5">
+            <h3 class="selectable" title="Devs & Dragons">Devs & Dragons</h3>
+            <h3 class="selectable" title="Keepr">Keepr</h3>
+            <h3 class="selectable" title="AllSpice">AllSpice</h3>
           </div>
         </div>
-
-        <div class="row mt-3" id="keepr">
-      <div class="col-6">
-        <div class="ms-4 mb-5">
-          <img class="img-fluid keepr-img" src="https://figmage.com/images/KndD8m4gDG7hjt1xgq5lo.jpg" alt="Keepr">
-        </div>
       </div>
-
-      <div class="col-6">
-        <div class="me-4 mb-3 text-center text-white">
-          <h3>Keepr</h3>
-          <p>A full-stack web application similar to Pintrest I built over the course of a week. Users are able to upload
-            and share photos, save them in public and private vaults, and display their favorites on their profile page.
-            This application utilizes an auth0 authenticator, requiring users to create an account and login to upload
-            and save photos. Users can still browse public photos and vaults without logging in.
-          </p>
-
-          <p>This application was built with a Vue.Js front end and a .Net/MySQL back end, and included API testing
-            with Postman, mobile optimization, and accessibility/best practices testing with Lighthouse.</p>
-
-
-        </div>
-
-        <div class="sm-h-line mt-4"></div>
-
-        <div class="d-flex justify-content-between align-items-center mx-2 mt-4">
-          <img class="img-fluid html-icon2" src="https://cdn3d.iconscout.com/3d/free/thumb/free-html-5728485-4781249.png" alt="HTML" title="HTML5">
-          <img class="img fluid css-icon2" src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" title="CSS3">
-          <img class="img fluid js-icon2" src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript" title="JavaScript">
-          <img class="img fluid vue-icon2" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/367_Vuejs_logo-512.png" alt="VueJs" title="VueJs">
-          <img class="img fluid mysql-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/181/MySQL-512.png" alt="MySQL" title="MySQL">
-          <img class="img fluid csharp-icon" src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png" alt="C#" title="C#">
-        </div>
-
-        </div>
-      </div>
-
-        <div class="row">
-          <div class="col-12 m-auto my-5">
-            <div class="full-h-line"></div>
+  
+  <!-- NOTE have it cycle through multiple imgs of apps here would be cool -->
+      <div class="row mt-3" id="devs&dragons">
+        <div class="col-6">
+          <div class="ms-4 mb-5">
+            <img class="img-fluid" src="../assets/img/Screenshot (2).png" alt="Devs & Dragons">
           </div>
         </div>
+  
+        <div class="col-6">
+          <div class="me-4 mb-3 text-white">
+            <h3 class="text-center mb-3">Devs & Dragons</h3>
+            <p class="mb-4">A Dungeons & Dragons campaign management application built with a Vue.Js front
+            end and an Express/NodeJs back end that allows players to stay connected and coordinate their games.
+            </p>
+  
+            <p class="mb-4">This application was built by a team of five developers utilizing Scrum and Agile practices to coordinate our organization and communication throughout the build process.</p>
 
-      <div class="row mt-3" id="AllSpice">
-      <div class="col-6">
-        <div class="ms-4 mb-5">
-          <img class="img-fluid allspice-img" src="https://codeworks.blob.core.windows.net/public/assets/img/projects/AllSpicePoster.jpg" alt="Keepr">
-        </div>
-      </div>
+            <p class="mb-4">In addition to using MongoDB to store user data, this application also implements the D&D API to quickly and seamlessly provide
+            users with information on the vast number of items within the game</p>
+          </div>
 
-      <div class="col-6 mt-5">
-        <div class="me-4 mb-3 text-center text-white">
-          <h3>AllSpice</h3>
-          <p>A full stack recipe management web application I built over the course of a week. Similar to recipe apps
-            from Home Chef or Dinnerly, users are able to create recipes once logged in. They can add ingredients and
-            preperation instructions, save their favorite recipes, and filter recipes by type.
-            User authentication is handled by auth0, requiring an account to create or save any recipes or ingredients.
-            Recipes can still be viewed without an account.
-          </p>
-
-          <p>This application was built using a Vue.Js front end and a .Net/MySQL back end, and the API was tested
-            with Postman.</p>
-        </div>
-
-        <div class="sm-h-line mt-4"></div>
-
-        <div class="d-flex justify-content-between align-items-center mx-2 mt-4">
-          <img class="img-fluid html-icon2" src="https://cdn3d.iconscout.com/3d/free/thumb/free-html-5728485-4781249.png" alt="HTML" title="HTML5">
-          <img class="img fluid css-icon2" src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" title="CSS3">
-          <img class="img fluid js-icon2" src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript" title="JavaScript">
-          <img class="img fluid vue-icon2" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/367_Vuejs_logo-512.png" alt="VueJs" title="VueJs">
-          <img class="img fluid mysql-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/181/MySQL-512.png" alt="MySQL" title="MySQL">
-          <img class="img fluid csharp-icon" src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png" alt="C#" title="C#">
-        </div>
-
-        </div>
-      </div>
-
-        <div class="row">
-          <div class="col-12 m-auto my-5">
-            <div class="full-h-line"></div>
+  
+          <div class="sm-h-line"></div>
+  
+          <div class="d-flex justify-content-between align-items-center mx-5 mt-4">
+            <!-- <img class="img-fluid html-icon" src="https://cdn3d.iconscout.com/3d/free/thumb/free-html-5728485-4781249.png" alt="HTML" title="HTML5">
+            <img class="img fluid css-icon" src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" title="CSS3">
+            <img class="img fluid js-icon" src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript" title="JavaScript"> -->
+            <img class="img fluid vue-icon" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/367_Vuejs_logo-512.png" alt="VueJs" title="VueJs">
+            <img class="img fluid node-icon" src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" alt="NodeJs" title="NodeJS/Express">
+            <img class="img fluid mongo-icon" src="https://icons-for-free.com/iconfiles/png/512/MongoDB-1329545826074381322.png" alt="MongoDB" title="MongoDB">
+            <img class="img fluid post-icon" src="https://www.svgrepo.com/show/354202/postman-icon.svg" alt="Postman" title="Postman">
           </div>
         </div>
+      </div>
+  
+          <div class="row">
+            <div class="col-12 m-auto my-5">
+              <div class="full-h-line"></div>
+            </div>
+          </div>
+  
+          <div class="row mt-3" id="keepr">
+            
+            <div class="col-6">
+              <div class="ms-4 mb-3 text-white">
+            <h3 class="text-center mb-3">Keepr</h3>
+            <p>An application similar to Pintrest built in Vue.Js, .Net, and MySQL where users can manage their keeps and vaults, and
+              share them with others.
+            </p>
+
+            <p>This application utilizes an Auth0 authenticator to verify and manage user accounts. The UI is updated
+                accordingly based upon a user’s authentication.
+                </p>
+
+            <p>Built independently, I preformed API testing with Postman, accessibility/best practices testing with
+              Lighthouse, and optimized the site for mobile view.
+            </p>
+            
+            <p></p>
+  
+            
+          </div>
+          
+          <div class="sm-h-line mt-4"></div>
+          
+          <div class="d-flex justify-content-between align-items-center mx-5 mt-4">
+            <!-- <img class="img-fluid html-icon2" src="https://cdn3d.iconscout.com/3d/free/thumb/free-html-5728485-4781249.png" alt="HTML" title="HTML5">
+              <img class="img fluid css-icon2" src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" title="CSS3">
+              <img class="img fluid js-icon2" src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript" title="JavaScript"> -->
+              <img class="img fluid vue-icon2" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/367_Vuejs_logo-512.png" alt="VueJs" title="VueJs">
+              <img class="img fluid mysql-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/181/MySQL-512.png" alt="MySQL" title="MySQL">
+              <img class="img fluid csharp-icon" src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png" alt="C#" title="C#">
+              <img class="img fluid post-icon2" src="https://www.svgrepo.com/show/354202/postman-icon.svg" alt="Postman" title="Postman">
+            </div>
+            
+          </div>
+          <div class="col-6">
+            <div class="me-4 mb-5">
+              <img class="img-fluid keepr-img" src="https://figmage.com/images/KndD8m4gDG7hjt1xgq5lo.jpg" alt="Keepr">
+            </div>
+          </div>
+        </div>
+  
+          <div class="row">
+            <div class="col-12 m-auto my-5">
+              <div class="full-h-line"></div>
+            </div>
+          </div>
+  
+        <div class="row mt-3" id="AllSpice">
+        <div class="col-6">
+          <div class="ms-4 mb-5">
+            <img class="img-fluid allspice-img" src="https://codeworks.blob.core.windows.net/public/assets/img/projects/AllSpicePoster.jpg" alt="Keepr">
+          </div>
+        </div>
+  
+        <div class="col-6 mt-5">
+          <div class="me-4 mb-3 text-white">
+            <h3 class="text-center">AllSpice</h3>
+            <p>A recipe management application built independently using a Vue.Js front end and a .Net/MySQL back end where users
+              are able to add and view recipes
+            </p>
+  
+            <p>Users can filter recipes by type, add or edit ingredients and instructions, and save their favorite recipes to
+              their account</p>
+
+              <p>This application's API was thoroughly tested with Postman, and has been optimized for mobile view.</p>
+          </div>
+  
+          <div class="sm-h-line mt-4"></div>
+  
+          <div class="d-flex justify-content-between align-items-center mx-5 mt-4">
+            <!-- <img class="img-fluid html-icon2" src="https://cdn3d.iconscout.com/3d/free/thumb/free-html-5728485-4781249.png" alt="HTML" title="HTML5">
+            <img class="img fluid css-icon2" src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" alt="CSS" title="CSS3">
+            <img class="img fluid js-icon2" src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="JavaScript" title="JavaScript"> -->
+            <img class="img fluid vue-icon2" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/367_Vuejs_logo-512.png" alt="VueJs" title="VueJs">
+            <img class="img fluid mysql-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/181/MySQL-512.png" alt="MySQL" title="MySQL">
+            <img class="img fluid csharp-icon" src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png" alt="C#" title="C#">
+            <img class="img fluid post-icon2" src="https://www.svgrepo.com/show/354202/postman-icon.svg" alt="Postman" title="Postman">
+          </div>
+  
+          </div>
+        </div>
+  
+          <div class="row">
+            <div class="col-12 m-auto my-5">
+              <div class="full-h-line"></div>
+            </div>
+          </div>
+
+    </div>
 
     <!-- SECTION ABOUT ME -->
 
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-5 text-center">
         <img class="img-fluid avatar" src="../assets/img/Professional Photo.jpg" alt="Andrew Allison">
       </div>
@@ -172,13 +208,13 @@
       <div class="col-12 m-auto my-5">
         <div class="h-line"></div>
       </div>
-    </div>
+    </div> -->
 
     <!-- SECTION FORM -->
 
     <div class="row">
       <!-- TODO set up formsubmit path to send emails to me -->
-      <div class="col-6 m-auto">
+      <div class="col-8 m-auto">
         <div class="form card p-4 ms-5 mt-1 mb-5">
           <h3 class="fs-1 text-black text-center mb-4">Get In Touch</h3>
           <form action="https://formsubmit.co/7821106b1e4ce3ae17cfdca1e845b808" method="POST">
@@ -206,14 +242,14 @@
         </div>
       </div>
 
-      <div class="col-6 text-white">
+      <!-- <div class="col-6 text-white">
         <div>
           <h3 class="fs-1 text-center">Connect</h3>
           <div class="mt-3 justify-content-center">
             <GlowingIcons />
           </div>
         </div>
-      </div>
+      </div> -->
 
 
     </div>
@@ -246,12 +282,26 @@ export default {
   background-image: url('https://static.vecteezy.com/system/resources/previews/002/821/966/original/abstract-triangular-geometric-background-free-vector.jpg');
   background-size: cover;
   background-repeat: no-repeat;
-  height: 100vh;
+  height: 91.5vh;
+}
+
+.text-shadow{
+  text-shadow: 1px 1px gray;
 }
 .h-line {
   height: 1px;
   width: 100%;
-  background: white;
+  background: black;
+}
+
+.background-sec {
+  background: rgb(25,96,98);
+background: linear-gradient(90deg, rgba(25,96,98,1) 0%, rgba(7,53,69,1) 35%, rgba(25,96,98,1) 100%);
+}
+
+.background-third {
+  background: rgb(20,182,197);
+background: linear-gradient(90deg, rgba(20,182,197,1) 0%, rgba(8,56,72,1) 50%, rgba(20,86,96,1) 100%);
 }
 
 .full-h-line {
@@ -287,11 +337,22 @@ export default {
   height: 6em;
 }
 
+.mongo-icon{
+  height: 7em;
+}
+
+.post-icon{
+  height: 7em
+}
+.post-icon2{
+  height: 7em
+}
+
 .vue-icon {
   height: 8em;
 }
 .vue-icon2 {
-  height: 6em;
+  height: 8em;
 }
 
 .node-icon {
@@ -299,11 +360,11 @@ export default {
 }
 
 .mysql-icon {
-  height: 6em;
+  height: 8em;
 }
 
 .csharp-icon {
-  height: 6em;
+  height: 7em;
 }
 
 
