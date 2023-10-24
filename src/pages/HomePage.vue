cd9a2d116b5fc13d8969a3824b7973d6e7fe9b1a
Andrew Allison <andrewallison5211@gmail.com>
2023-10-24 13:55:35 -0600

    design looks good

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid bg-black">
 
     <div class="background-img">
       <div class="row">
@@ -9,8 +9,8 @@
       </div>
   
       <div class="row mb-5">
-        <div class="col-6 m-auto mt-5 me-5">
-          <div class="mt-5 ms-5 text-black typewriter">
+        <div class="col-6 m-auto mt-5 me-5 pt-5">
+          <div class="mt-5 ms-5 text-black typewriter pt-5">
             <h1 class="fs-5 mt-5 text-shadow">Hi, I'm Andrew</h1>
               <h1 class="fs-5 text-shadow">A full-stack <span class="fs-1 text-info">Software Developer</span></h1>
           </div>
