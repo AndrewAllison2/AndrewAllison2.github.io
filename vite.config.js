21fab40caf7cc4e8372fc23f2e4ab89c57c017fb
Andrew Allison <andrewallison5211@gmail.com>
2023-11-03 11:19:13 -0600

    env update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/landing/',
+  base: '',
   plugins: [vue()],
   build: {
-    outDir: '../landing',
+    outDir: '',
     sourcemap: false
   },
   server: {
